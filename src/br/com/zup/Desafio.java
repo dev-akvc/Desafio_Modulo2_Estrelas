9bc33d1c74347fd8f08d86cc8d9bfdda807f483f
Anna Karolyne <dev.akvc@gmail.com>
2021-09-03 13:03:45 -0300

    Entrega Média

--- src/br/com/zup/Desafio.java
+++ src/br/com/zup/Desafio.java
@@ -11,33 +11,85 @@ public class Desafio {
 Crie um programa para gerenciar uma lista de funcionários de uma empresa,
 cada funcionário tem nome, telefone, email e CPF. No final do programa deve ser
 exibida a lista de funcionários cadastrados.
-Entrega Mínima: O sistema permite, via terminal, inserir pelo menos 1 funcionário
-em uma lista e retornar o mesmo na tela.
+Entrega Média: O sistema permite adicionar mais de um funcionário e apresenta
+um menu para decidir se deverá:
+1. Adicionar mais funcionários.
+2. Exibir a lista de todos os funcionários cadastrados.
+3. Encerrar o programa.
  */
 //        Instanciando Scanner
         Scanner leitor = new Scanner(System.in);
 
 //        Declarando variáveis
-        String nomeFuncionarix, telefoneFuncionarix, emailFuncionarix, cpfFuncionarix;
+        String nomeFuncionarix = null, telefoneFuncionarix = null, emailFuncionarix = null, cpfFuncionarix = null, menuInicial;
         Map<String, String> funcionarixs = new HashMap<String, String>();
+        int opcaoEscolhida;
+        boolean opcao = true;
 
-//        Cadastrando funcionárixs
         System.out.println("** Bem vinde ao sistema de funcionárixs **");
-        System.out.println("Para cadastrar funcionárix, informe os dados a seguir: ");
-        System.out.println("Nome: ");
-        nomeFuncionarix = leitor.nextLine();
-        System.out.println("Telefone: ");
-        telefoneFuncionarix = leitor.nextLine();
-        System.out.println("E-mail: ");
-        emailFuncionarix = leitor.nextLine();
-        System.out.println("CPF: ");
-        cpfFuncionarix = leitor.nextLine();
+//        Menu Inicial
+        menuInicial = "O que você gostaria de fazer?\n1- Adicionar funcionárixs\n2- Exibir lista de funcionárixs cadastradxs\n3- Encerrar o programa";
 
-        funcionarixs.put(cpfFuncionarix, " Nome: " +nomeFuncionarix+ " Telefone: " +telefoneFuncionarix+ " E-mail: " +emailFuncionarix);
-        System.out.println(funcionarixs);
+//        Para reutilizar Menu Inicial
+        System.out.println(menuInicial);
+        opcaoEscolhida = leitor.nextInt();
+        leitor.nextLine();
+
+        while (opcao) {
+
+            switch (opcaoEscolhida) {
+                case 1:
+                //        Cadastrando funcionárixs
+                    System.out.println("Informe o nome: ");
+                    nomeFuncionarix = leitor.nextLine();
+                    System.out.println("Informe o telefone: ");
+                    telefoneFuncionarix = leitor.nextLine();
+                    System.out.println("Informe o e-mail: ");
+                    emailFuncionarix = leitor.nextLine();
+                    System.out.println("Informe o CPF: ");
+                    cpfFuncionarix = leitor.nextLine();
+                    funcionarixs.put(cpfFuncionarix, " Nome: " + nomeFuncionarix + " Telefone: " + telefoneFuncionarix + " E-mail: " + emailFuncionarix);
+
+                    //        Confirmação de cadastro
+                    System.out.println("Cadastro realizado!");
+
+                    System.out.println(menuInicial);
+                    opcaoEscolhida = leitor.nextInt();
+                    leitor.nextLine();
+
+                    break;
+
+                case 2:
+
+                    if (funcionarixs.size() != 0) {
+
+                        for (String chaveFuncionario : funcionarixs.keySet()) {
+                            System.out.println("Funcionário: " + funcionarixs.get(chaveFuncionario) + " CPF:" + chaveFuncionario);
+                        }
+                    } else {
+                        System.out.println("Lista está vazia! Cadastre funcionárixs.");
+                    }
+
+                    System.out.println(menuInicial);
+                    opcaoEscolhida = leitor.nextInt();
+                    leitor.nextLine();
+
+                    break;
+
+                case 3:
+                    System.out.println("Cadastro encerrado.");
+                    opcao = false;
+                    break;
+
+                default:
+                    System.out.println("Digite uma opção válida.");
+                    System.out.println(menuInicial);
+                    opcaoEscolhida = leitor.nextInt();
+                    leitor.nextLine();
+                    break;
 
 
-
-
+            }
+        }
     }
 }
