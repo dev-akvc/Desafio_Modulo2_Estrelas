46c4e298387f42eadc49535a023ae94edd79f39f
Anna Karolyne <dev.akvc@gmail.com>
2021-09-03 11:00:08 -0300

    Primeiro commit

---
+++ src/br/com/zup/Desafio.java
@@ -0,0 +1,20 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Desafio {
+
+    public static void main(String[] args) {
+/*
+Crie um programa para gerenciar uma lista de funcionários de uma empresa,
+cada funcionário tem nome, telefone, email e CPF. No final do programa deve ser
+exibida a lista de funcionários cadastrados.
+Entrega Mínima: O sistema permite, via terminal, inserir pelo menos 1 funcionário
+em uma lista e retornar o mesmo na tela.
+ */
+//        Instanciando Scanner
+        Scanner leitor = new Scanner(System.in);
+
+
+    }
+}
